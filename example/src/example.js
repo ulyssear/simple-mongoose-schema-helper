7905862f00b0c44190e4f00434f62d21bcad7c71
Ulysse ARNAUD <mexanga@gmail.com>
2021-07-04 12:24:50 +0200

    Update example.js

--- example/src/example.js
+++ example/src/example.js
@@ -1,7 +1,7 @@
 
 document.addEventListener(event => {
 
-    const my_element = `<h1>Ceci est un titre</h1>`
+    const my_element = render`<h1>Ceci est un titre</h1>`
     document.appendChild(my_element)
 
 })
