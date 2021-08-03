d5d3dc8048e91c50087cfad57f289c660063e24b
Ulysse Arnaud <ulysse.arnaud@liigem.io>
2021-08-03 08:53:57 +0200

    Now builds commonjs and umd files

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ Import the package inside your project.
 
 
 ```js
-// You have the choice between these two lines below.
-
 import render from 'just-render'
 ```
 
@@ -29,9 +27,3 @@ const my_element = render`<h1>Hello world !</h1>`
 const name = 'world';
 const my_element = render`<h1>Hello ${name} !</h1>`
 ```
-
-### Some things to change
-
-* Replace `laravel-mix` package by Webpack (or another one like esbuild) and Babel.
-  
-    The package `laravel-mix` is a package which permits to use webpack without adding configurations (all are already prepare by the package).
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 export function render(content, ...datas) {
     if (typeof content === 'object') {
-        content = Array.from(content);
+        content = Array.from(content)
 
         for (let index in content) {
-            content[index] = content[index].replace("\r\n", "\n").split("\n");
+            content[index] = content[index].replace("\r\n", "\n").split("\n")
 
             for (let line in content[index]) {
-                content[index][line] = content[index][line].trim();
+                content[index][line] = content[index][line].trim()
             }
 
-            content[index] = content[index].join('').trim();
+            content[index] = content[index].join('').trim()
 
-            let data = datas.shift();
+            let data = datas.shift()
 
-            if (undefined !== data) content[index] += data;
+            if (undefined !== data) content[index] += data
         }
 
-        content = content.join('');
+        content = content.join('')
     }
 
-    const template = document.createElement('template');
-    template.innerHTML = content;
+    const template = document.createElement('template')
+    template.innerHTML = content
 
-    return template.content.cloneNode(true);
+    return template.content.cloneNode(true)
 }
+
+export default render
