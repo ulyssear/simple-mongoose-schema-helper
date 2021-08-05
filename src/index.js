c68930d305c9779d28f9b18085b7dc03f41f885b
Ulysse Arnaud <ulysse.arnaud@liigem.io>
2021-08-05 11:31:26 +0200

    Minor fixes (no space between words when using variables)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ module.exports = function render(content, ...datas) {
                 content[index][line] = content[index][line].trim()
             }
 
-            content[index] = content[index].join(' ').trim()
+            content[index] = content[index].join(' ')
 
             let data = datas.shift()
 
