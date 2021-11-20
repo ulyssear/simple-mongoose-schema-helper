be86c8be175192695845cc0fb2b9c1099c2f79ce
Ulysse ARNAUD <ulysse.arnaud@liigem.io>
2021-11-20 18:25:21 +0100

    Reduced lines & updated npm config

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 module.exports = function render(content, ...datas) {
-    if (typeof content === 'object') {
-        content = Array.from(content)
+    if ('object' === typeof content) {
+        let data;
+
+        content = Array.from(content);
 
         for (let index in content) {
-            content[index] = content[index].replace("\r\n", "\n").split("\n")
-
-            for (let line in content[index]) {
-                content[index][line] = content[index][line]
-            }
-
-            content[index] = content[index].join(' ')
-
-            let data = datas.shift()
-
-            if (undefined !== data) content[index] += data
+            content[index] = content[index].replace("\r\n", "\n").split("\n");
+            for (let line in content[index]) content[index][line] = content[index][line];
+            content[index] = content[index].join(' ');
+            if (undefined !== (data = datas.shift())) content[index] += data;
         }
 
-        content = content.join(' ').trim()
+        content = content.join(' ').trim();
     }
 
-    const template = document.createElement('template')
-    template.innerHTML = content
+    const template = document.createElement('template');
+    template.innerHTML = content;
 
-    const fragment = template.content.cloneNode(true)
+    const fragment = template.content.cloneNode(true);
 
-    return [fragment.firstChild, fragment][+(1 < fragment.childNodes)]
+    return [fragment.firstChild, fragment][+(1 < fragment.childNodes)];
 }
