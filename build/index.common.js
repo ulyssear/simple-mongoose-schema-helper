5c4367b24dcf53da4653fee9d0292f49dd36e6f8
Ulysse ARNAUD <ulysse.arnaud@liigem.io>
2021-12-23 21:07:14 +0100

    Initial commit

--- build/index.common.js
+++ build/index.common.js
@@ -1,30 +1,21 @@
 'use strict';
 
-module.exports = function render(content, ...datas) {
-    if (typeof content === 'object') {
-        content = Array.from(content);
-
-        for (let index in content) {
-            content[index] = content[index].replace("\r\n", "\n").split("\n");
-
-            for (let line in content[index]) {
-                content[index][line] = content[index][line];
-            }
-
-            content[index] = content[index].join(' ');
-
-            let data = datas.shift();
-
-            if (undefined !== data) content[index] += data;
-        }
-
-        content = content.join(' ').trim();
+module.exports = {
+    _temp: {},
+    type(type) {
+        this._temp = {type, ...this._temp};
+        return this;
+    },
+    required(required = true) {
+        this._temp = {required, ...this._temp};
+        return this;
+    },
+    get() {
+        const value = this._temp;
+        this._clean();
+        return value;
+    },
+    _clean() {
+        this._temp = {};
     }
-
-    const template = document.createElement('template');
-    template.innerHTML = content;
-
-    const fragment = template.content.cloneNode(true);
-
-    return [fragment.firstChild, fragment][+(1 < fragment.childNodes)]
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-module.exports = function render(content, ...datas) {
-    if ('object' === typeof content) {
-        let data;
-
-        content = Array.from(content);
-
-        for (let index in content) {
-            content[index] = content[index].replace("\r\n", "\n").split("\n");
-            for (let line in content[index]) content[index][line] = content[index][line];
-            content[index] = content[index].join(' ');
-            if (undefined !== (data = datas.shift())) content[index] += data;
-        }
-
-        content = content.join(' ').trim();
+module.exports = {
+    _temp: {},
+    type(type) {
+        this._temp = {type, ...this._temp};
+        return this;
+    },
+    required(required = true) {
+        this._temp = {required, ...this._temp};
+        return this;
+    },
+    get() {
+        const value = this._temp;
+        this._clean();
+        return value;
+    },
+    _clean() {
+        this._temp = {};
     }
-
-    const template = document.createElement('template');
-    template.innerHTML = content;
-
-    const fragment = template.content.cloneNode(true);
-
-    return [fragment.firstChild, fragment][+(1 < fragment.childNodes)];
 }
